500e7b663bd105ef7d8bc260cb07595c8d479171
uzairansari11 <uzairans532@gmail.com>
2023-12-16 20:28:18 +0530

    fix logo

--- frontend/src/component/Navbar.jsx
+++ frontend/src/component/Navbar.jsx
@@ -10,11 +10,11 @@ import { useDispatch } from "react-redux";
 import { cookiesGetter } from "../utils/cookies.js";
 import { logoutUserFromApi } from "../redux/authentication/action.js";
 import { userType } from "../constant/constant.js";
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
   const dispatch = useDispatch();
   const { userDetails } = useSelector((store) => store.authenticationReducer);
-const navigate=useNavigate()
+  const navigate = useNavigate();
 
   const handleLogout = async () => {
     const token = cookiesGetter();
@@ -29,7 +29,7 @@ const navigate=useNavigate()
         }
       );
       dispatch(logoutUserFromApi());
-      navigate("/",{replace:true})
+      navigate("/", { replace: true });
       toast.success(response?.data?.message);
     } catch (error) {
       toast.error(error?.response?.data);
@@ -47,26 +47,19 @@ const navigate=useNavigate()
       justifyContent={"space-between"}
       overflowY="hidden"
     >
-      <Link
-        to={
-          userDetails === userType.customer
-            ? "/dashboard/products"
-            : "/dashboard/manager/products"
-        }
+      <Box
+        width={"20"}
+        display="flex"
+        justifyContent={"center"}
+        alignItems={"center"}
       >
-        <Box
-          width={"20"}
-          display="flex"
-          justifyContent={"center"}
-          alignItems={"center"}
-        >
-          <Image
-            src="https://assets-global.website-files.com/6284afcd3c8fe34dca52d136/62bfd7e69c25897f423bcdac_TechEagle%20new%20logo.svg"
-            alt="logo"
-            width={"100%"}
-          />
-        </Box>
-      </Link>
+        <Image
+          src="https://assets-global.website-files.com/6284afcd3c8fe34dca52d136/62bfd7e69c25897f423bcdac_TechEagle%20new%20logo.svg"
+          alt="logo"
+          width={"100%"}
+        />
+      </Box>
+
       <Box>
         {userDetails ? (
           <MenuOptions logout={handleLogout}>
